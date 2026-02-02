ff7a0d8f978ea65dcdda914fb436f1261b810f42
Raj Singh <rajsinghdurgapur26@gmail.com>
2026-02-02 08:06:01 +0530

    minor changes

--- Frontend/birthday/src/Components/BirthdayCard.jsx
+++ Frontend/birthday/src/Components/BirthdayCard.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { FaPlay } from "react-icons/fa";
-import song from "../Assets/birthday-song.mp3.mp3";
+import song from "../Assets/birthday-song.mp3";
 
 const BirthdayCard = () => {
   const { state } = useLocation();
